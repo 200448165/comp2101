d73c22f1625eb738085a172ec258bbe6f882c05b
Sreerag Das <200448165@student.georgianc.on.ca>
2021-02-11 11:50:24 -0500

    Lab1

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -22,7 +22,7 @@ echo ""
 # Modifying on the basis of Task1
 echo "12 Largest Lines Are:"
 echo "============="
-find / -type f -exec ls -alh {} + 2>/dev/null | sort -k5 -hr| head -n -12 | awk '{print $3,$5,$9}'
+find / 2>/dev/null -type f -exec ls -alh {} + | sort -k5 -hr| head -n -12 | awk '{print $3,$5,$9}'
 echo ""
 # for the task, add
 # commands to display a title
